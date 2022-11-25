e80571362e4923816b179130eb16cfb8de0c5516
Grace-aloo <graceaaloo@gmail.com>
2022-11-25 19:38:44 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+ function writeCards(names, eventName){
+    let newArr = [];
+    for(let i = 0;i < names.length; i++){
+      newArr.push (`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+        }
+    return newArr;
+ }
+ writeCards(['Guadalupe','Ollie','Aki'],"surprise");
+ function countDown(){
+    let countDown =10
+    while( countDown >= 0 ){
+        console.log(countDown--)
+    }
+ }
